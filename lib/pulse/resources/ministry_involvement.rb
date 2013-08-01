facd61b65c73e17bdfe61643375f88ff7917ef4f
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-08-01 12:23:31 -0400

    Return some user meta data for convenience

--- lib/pulse/resources/ministry_involvement.rb
+++ lib/pulse/resources/ministry_involvement.rb
@@ -4,8 +4,21 @@ module Pulse
 
     class << self
       def build_from(resp, request_params = {})
-        resp = resp.is_a?(Array) ? resp.first['ministry_involvements']['ministry_involvement'] : resp
-        super(resp, request_params)
+        if resp.is_a?(Array)
+          new_resp = resp.first['ministry_involvements']['ministry_involvement']
+
+          # Include some user meta data for convenience
+          ['first_name', 'last_name', 'email', 'guid', 'civicrm_id'].each do |user_attribute|
+            new_resp.each do |mi|
+              mi['user'] ||= {}
+              mi['user'][user_attribute] = resp.first['ministry_involvements'][user_attribute]
+            end
+          end
+        else
+          new_resp = resp
+        end
+
+        super(new_resp, request_params)
       end
     end
 
